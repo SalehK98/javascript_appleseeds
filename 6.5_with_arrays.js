cbb4bea562524d85dc2acd19b10d4a0efa10e802
Saleh Kalouti <salehkalouti@gmail.com>
2023-01-12 19:00:55 +0200

    object tasks

--- 6.5_with_arrays.js
+++ 6.5_with_arrays.js
@@ -1,25 +1,66 @@
-// 1
+// 1 fill method
 // const arr = new Array(100);
 const myObj = { name: "saleh" };
 const arr = Array(100).fill(myObj);
-// 2
+
+// --------------------------------------------------------------
+console.log("------------------------------------------------------------   2");
+
+// 2 from array method
 const arr1 = Array.from(Array(100), (el, idx) => {
   return idx + 1;
 });
 console.log(arr1);
-// 3
 
-// 4
+// ------------------------------------------------------------
+console.log("------------------------------------------------------------   3");
 
-// 5
+// 3 object values to array
+const myObj1 = {
+  name: "saleh",
+  age: 24,
+  height: 174,
+};
+
+const arr4 = [...Object.values(myObj1)];
+// const arr4 = Object.values(myObj1); this works as well
+console.log(arr4);
+
+// -----------------------------------------------------------------
+console.log("------------------------------------------------------------   4");
+
+// 4 array to object
+
+const arr5 = ["saleh", "dana", "kalouti"];
+// const myObj2 = Object.assign({}, arr5);
+const myObj2 = { ...arr5 };
+// const myObj2 = arr5.reduce((a, v) => ({ ...a, [v]: v }), {});
+
+console.log(myObj2);
+// -----------------------------------------------------------------
+console.log(
+  "------------------------------------------------------------    5"
+);
+
+// 5 is array
 console.log(Array.isArray(arr1));
 
-// 6
-const arr2 = [];
+// ---------------------------------------------------------------------
+console.log(
+  "------------------------------------------------------------    6"
+);
+
+// 6 copies pf arrays
+const arr2 = [2, 5, 7];
+
+// these two ways to copy arrays by value
 const adder = (arr) => {
   for (let i = 0; i < arr.length; i++) arr2.push(arr[i]);
 };
+const arr6 = arr2.slice(0);
 
+const arr7 = [...arr2];
+// this copies array by reference
 const arr3 = arr;
 
 // const arr = ["df", "fg"];
@@ -31,5 +72,6 @@ const array2 = array.slice(1, 3);
 console.log(array);
 console.log(array2);
 array.pop();
+array2[0] = 88;
 console.log(array);
 console.log(array2);
--- 8.2_maps_vs_objects_preformnace.js
+++ 8.2_maps_vs_objects_preformnace.js
@@ -1,9 +1,43 @@
+console.time("object key, value assignment");
 const myObj = {};
-const myMap = console.time("my operation");
 for (let i = 0; i < 100000; i++) {
   myObj[i] = i;
-  //   console.log(myObj);
 }
-myObj[88] = 4;
-// console.log(myObj[55]);
-console.timeEnd("my operation");
+console.timeEnd("object key, value assignment");
+// ---------------------------------------------------
+console.time("map object key, value assignment");
+const myMap = new Map();
+for (let i = 0; i < 100000; i++) {
+  myMap.set(i, i);
+}
+console.timeEnd("map object key, value assignment");
+
+// finding from itself --------------------------------------------------------------------
+
+console.time("finding from it self: object");
+myObj[5985];
+console.timeEnd("finding from it self: object");
+// ---------------------------------------
+console.time("finding from it self: map");
+myMap.get(5985);
+console.timeEnd("finding from it self: map");
+
+// adding an entry--------------------------------------------------------------
+
+console.time("adding an entry self: object");
+myObj[1000000] = 1000000;
+console.timeEnd("adding an entry self: object");
+// -----------------------------------------
+console.time("adding an entry self: map");
+console.timeEnd("adding an entry self: map");
+myMap.set(1000000, 1000000);
+
+// deleting an entry -------------------------------------------------------------
+
+console.time("deleting an entry: object");
+delete myObj[1000000];
+console.timeEnd("deleting an entry: object");
+// -------------------------------------------
+console.time("deleting an entry: map");
+myMap.delete(1000000);
+console.timeEnd("deleting an entry: map");
